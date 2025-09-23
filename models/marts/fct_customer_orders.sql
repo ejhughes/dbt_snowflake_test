498a0542840be077a22f6fc5a14c77cedde6fbae
Erica Hughes <erica.hughes@theinformationlab.com>
2025-09-23 21:47:58 +0000

    Converted customer_orders CTE to window functions in final CTE

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -23,17 +23,6 @@ payments as (
 
 -- Logical CTEs
 
-completed_payments. as(
-
-    select
-        orderid as order_id,
-        max(created) as payment_finalized_date,
-        sum(amount) / 100.0 as total_amount_paid
-    from payments
-    where status <> 'fail'
-    group by 1
-
-    )
 
 ---
 
@@ -54,16 +43,6 @@ paid_orders as (
     left join customers on orders.user_id = customers.id
 ),
 
-customer_orders as (
-    select
-        customers.id as customer_id,
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(orders.id) as number_of_orders
-    from customers
-    left join orders on orders.user_id = customers.id
-    group by 1
-),
 
 ---
 
@@ -87,9 +66,13 @@ final as(
 
         -- new vs returning customer
         case
-            when customers.first_order_date = completed_payments.order_placed_at 
-            then 'new' 
-            else 'return'
+            when (
+                rank() over(
+                    partition by customer_id
+                    order by order_placed_at, order_id
+                    ) = 1
+                ) then 'new'
+                else 'return'
         end as nvsr,
 
         -- customer lifetime value
@@ -99,7 +82,10 @@ final as(
         ) as customer_lifetime_value
 
         -- first order date
-        customers.first_order_date as fdos
+        first_value(paid_orders.order_placed_at) over(
+            partition by paid_orders.customer_id
+            order by paid_orders.order_placed_at
+        ) as fdos
 
     from paid_orders
 
