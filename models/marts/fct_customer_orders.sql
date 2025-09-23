9eef7cc7b81fc3d94a3fd71fbbf5efa39fceaf6c
Erica Hughes <erica.hughes@theinformationlab.com>
2025-09-23 21:41:44 +0000

    Creating final CTE and cleaning up table references

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -23,7 +23,7 @@ payments as (
 
 -- Logical CTEs
 
-completed_payments as(
+completed_payments. as(
 
     select
         orderid as order_id,
@@ -35,9 +35,7 @@ completed_payments as(
 
     )
 
--- Final CTE
-
--- Single Select Statement
+---
 
 
 paid_orders as (
@@ -46,60 +44,72 @@ paid_orders as (
         orders.user_id as customer_id,
         orders.order_date as order_placed_at,
         orders.status as order_status,
-        completed_paymentstotal_amount_paid,
-        completed_paymentspayment_finalized_date,
-        c.first_name as customer_first_name,
-        c.last_name as customer_last_name
+        completed_payments.total_amount_paid,
+        completed_payments.payment_finalized_date,
+        customers.first_name as customer_first_name,
+        customers.last_name as customer_last_name
     from orders
-    left join completed_payments
-    on orders.id = completed_paymentsorder_id
-    left join customers c on orders.user_id = c.id
+    left join completed_payments.
+    on orders.id = completed_payments.order_id
+    left join customers on orders.user_id = customers.id
 ),
 
 customer_orders as (
     select
-        c.id as customer_id,
+        customers.id as customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(orders.id) as number_of_orders
-    from customers c
-    left join orders on orders.user_id = c.id
+    from customers
+    left join orders on orders.user_id = customers.id
     group by 1
+),
+
+---
+
+-- Final CTE
+
+final as(
+    
+    select
+        completed payments.*,
+
+        -- sales transaction sequence
+        row_number() over (
+            order by completed_payments.order_id
+        ) as transaction_seq,
+
+        -- customer sales sequence
+        row_number() over (
+            partition by customer_id 
+            order by completed_payments.order_id
+        ) as customer_sales_seq,
+
+        -- new vs returning customer
+        case
+            when customers.first_order_date = completed_payments.order_placed_at 
+            then 'new' 
+            else 'return'
+        end as nvsr,
+
+        -- customer lifetime value
+        sum(total_amount_paid) over(
+            partition by paid_orders.customer_id
+            order by paid_orders.order_placed_at
+        ) as customer_lifetime_value
+
+        -- first order date
+        customers.first_order_date as fdos
+
+    from paid_orders
+
+    left join customer_orders as on paid_orders.customer_id = customer_orders.customer_id
+
+    order by order_id
 )
 
-select
-    completed payments.*,
+-- Single Select Statement
 
-    row_number() over (
-        order by completed_paymentsorder_id
-    ) as transaction_seq,
+select * from final
 
-    row_number() over (
-        partition by customer_id 
-        order by completed_paymentsorder_id
-    ) as customer_sales_seq,
 
-    case
-        when c.first_order_date = completed_paymentsorder_placed_at 
-        then 'new' 
-        else 'return'
-    end as nvsr,
-
-    x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos
-from paid_orders p
-
-left join customer_orders as c using (customer_id)
-left outer join(
-    select 
-        completed_paymentsorder_id, 
-        sum(t2.total_amount_paid) as clv_bad
-    from paid_orders p
-    left join paid_orders t2
-        on completed_paymentscustomer_id = t2.customer_id
-        and completed_paymentsorder_id >= t2.order_id
-    group by 1
-    order by completed_paymentsorder_id
-    ) x
-    on x.order_id = completed_paymentsorder_id
-order by order_id
